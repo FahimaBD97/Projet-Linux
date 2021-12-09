a56217198b3c934cf85fbfd1fd94f0eccfa36a6c
Etudiant <fahima.boudaoud@gmail.com>
2021-12-09 02:16:18 +0100

    commit

--- script.sh
+++ script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
+#recuperation des images
 function monsite {
 rm new.html
 touch new.html
-cat header.txt >>new.html
+cat header.txt >> new.html
 
 if [ -e "images/poney.jpeg" ]; then
-
-echo "<div class='esponsive'>
+echo "<div class='responsive'>
   <div class='gallery'>
     <a target='_blank' href='images/poney.jpeg' >
       <img src='images/poney.jpeg' alt=''  >
@@ -51,23 +50,70 @@ echo "<div class='responsive'>
   </div>
 </div> " >>  new.html
 fi
+cat footer.txt >> new.html
+
 firefox new.html
 
 }
 
-if [ $# -eq 0 ] ;then
-monsite
-fi
-
+#la fonction help
 function help {
-echo "mon help"
+echo "--help "
+echo "--build  /taper le chemain du dossier ou copier votre le script"
+echo "--build  /taper le chemain du dossier ou copier votre le script"
+echo "build --debug   pour créer un fichier qui vous affiche les lignes de l'executable "
+echo "connect  pour vous authentifier "
 }
 if [ $1 = "--help"  ] || [ $1 = "-h"  ]  ; then  
 help
 
 fi
- 
 
+# fonction pour deplacer les fichier du projet vers un nouveau dossier 
+
+function --build {
+
+cp script.sh "$1"
+cp header.txt "$1"
+cp new.html "$1"
+cp footer.txt "$1"
+cp -r images "$1"
+cp vitrin.html "$1"
+cd "$1"
+
+
+
+}
+
+if [ "$1" = "--build" ]; then
+--build "$2"
+fi
+
+if [ "$1" = "build --debug" ]; then
+command.txt 
+fi 
+
+#authentification 
+
+function connect{
+
+echo  "$entrez votre nom "
+
+read "$nom_utilisateur : " 
+echo "$entre votre mot de passe"
+
+read "$mot de passe : "
+}
+
+if [ "$1" = " connect" ]; then
+connect
+fi
+}
+
+#appel a la fonction mon site si ya plus de paramétre 
+if [ $# -eq 0 ] ;then
+monsite
+fi
 
 
 
